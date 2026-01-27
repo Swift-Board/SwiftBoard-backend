42ffbd392385e89613ae4fc7a2b6d3f4ba274da7
Opia12345 <preciousopia7@gmail.com>
2026-01-27 22:16:17 +0100

    chore: push

--- src/routes/rideRoutes.js
+++ src/routes/rideRoutes.js
@@ -16,10 +16,8 @@ router.get("/search", searchRides);
 
 router.get("/my-bookings", protect, getMyBookings);
 
-// Book seats - also before /:id
 router.patch("/:id/book", protect, bookSeats);
 
-// CRUD Endpoints - These must come AFTER specific routes
 router.post("/", createRide);
 
 router.route("/:id").get(getRideById).patch(updateRide).delete(deleteRide);
