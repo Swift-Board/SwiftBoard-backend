43b2cfc8fc44645dda1350900278b4f18f0f3816
Opia12345 <preciousopia7@gmail.com>
2026-02-08 22:59:11 +0100

    chore: removed cors

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ io.on("connection", (socket) => {
 });
 
 // Middleware
-app.use(cors({ origin: CLIENT_URL, credentials: true }));
+// app.use(cors({ origin: CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
